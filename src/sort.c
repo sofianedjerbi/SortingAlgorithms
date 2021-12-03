7c1260458a1a78923230db04bbe4106be8d4b3ad
Kugge <sofiane.djerbi38@gmail.com>
2021-12-03 03:14:09 +0100

    Coded radixsort and denomsort

--- src/sort.c
+++ src/sort.c
@@ -24,9 +24,6 @@ void cpyswap(void *a, void *b, size_t size) {
 }
 
 
-/* c native qsort */
-//void qsortc(size_t len, uint32_t *t);
-
 /* Sort an array t via quicksort
  * len: len(T)
  * We use aliasing and len instead of f and d. */
@@ -47,7 +44,7 @@ void qsortu(size_t len, uint32_t *t) {
     qsortu(len-n-1, t + n + 1);
 }
 
-/* Sort an array t via quicksort and HEURISTICS
+/* Sort an array t via quicksort and random heuristics
  * len: len(T)
  * We use aliasing and len instead of f and d. */
 void qsorth(size_t len, uint32_t *t) {
@@ -90,5 +87,69 @@ void qsortg(void *t, size_t len, size_t size,
     memcpy(t + n*size, x, size);
     qsortg(t, n, size, compare);
     qsortg(t + (n + 1)*size, len-n-1, size, compare);
+}
+
+/* Sort an array of 32 bit integer via counting sort. */
+void denomsort(size_t len, uint32_t *t, uint32_t max) {
+    // Creating the counting tab and init it to 0
+    uint32_t *count = calloc(max+1, sizeof(uint32_t)); 
+    // Output array
+    uint32_t *output = malloc(sizeof(uint32_t) * len); 
+    // Counting values
+    for (size_t i=0; i < len; i++)
+        count[t[i]]++;
+    for (size_t i=1; i <= max; i++)
+        count[i] += count[i-1];
+    // Sorting the array
+    for (size_t i=len; i >= 1; --i) {
+        count[t[i-1]]--;
+        output[count[t[i-1]]] = t[i-1];
+    }
+    // Swapping values
+    for (size_t i=0; i < len; i++)
+        swap(t + i, output + i);
+    free(count);
+    free(output);
+}
+
+/* Sort an array of 32 bit integer via counting sort BUT
+ * only considering b bits in position n, n+1, ..., n+b 
+ * (Starting from the left). And t[i] < 2^max */
+void bit_denomsort(size_t len, uint32_t *t, size_t b, size_t n, size_t max) {
+    // Mask for extracting bytes
+    uint32_t mask = (1 << b) - 1; // b times "1"
+    // Constant shift applied to all numbers
+    uint32_t shift = n*b;
+    // Creating the counting tab and init it to 0
+    uint32_t *count = calloc((1 << max) + 1, sizeof(uint32_t)); 
+    // Output array
+    uint32_t *output = malloc(sizeof(uint32_t) * len); 
+    // Counting values
+    for (size_t i=0; i < len; i++) {
+        uint32_t j = (t[i] << shift) & mask; 
+        count[j]++;
+    }
+    for (size_t i=1; i <= (1 << max); i++)
+        count[i] += count[i-1];
+   // Sorting the array 
+    for (size_t i=len; i >= 1; --i) {
+        uint32_t j = (t[i-1] << shift) & mask;
+        count[j]--;
+        output[count[j]] = t[i-1];
+    }
+    // Swapping values
+    for (size_t i=0; i < len; i++)
+        swap(t + i, output + i);
+    free(count);
+    free(output);
+}
+
+/* Implementing radixsort on basis b, with t[i] < 2^max for all i */
+void radixsort(size_t len, uint32_t *t, size_t b, size_t max) {
+    // Number of bytes (integer division so add 1 if a remainder exists)
+    size_t nlen = max/b;
+    nlen += max % b == 0 ? 0 : 1; // Check if remainder exists
+    for (size_t i=0; i < nlen; i++)
+        bit_denomsort(len, t, b, i, max);
 }
 
