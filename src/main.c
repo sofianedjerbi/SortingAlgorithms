a72e1f44d0f547bd25b8e9fa7be51cb3a8a7364d
Kugge <sofiane.djerbi38@gmail.com>
2021-12-03 10:32:48 +0100

    Added basic help

--- src/main.c
+++ src/main.c
@@ -1,11 +1,32 @@
+#include <stdlib.h>
 #include <stdint.h>
 #include <stdio.h>
-#include <stdlib.h>
 #include <time.h>
 
 #include "tests.h"
 #include "sort.h"
 
+
+void print_help(){
+    printf("Usage: ./out [sort] [min] [max] [inc] [pow]\n");
+    printf("    min: Min lenght of the test array\n");
+    printf("    max: Max lenght of the test array\n");
+    printf("    inc: Lenght incrementation of each test array\n");
+    printf("    pow: Max bits of each element of the array (< 32)\n");
+    printf("Sorts:\n");
+    printf("    -qc: Quicksort native sort\n");
+    printf("    -qu: Quicksort on u32, latest element as pivot\n");
+    printf("    -qg: Quicksort on u32, random pivot\n");
+    printf("    -qg: Quicksort replication of C native qsort (random pivot)\n");
+    printf("    -cs: Counting sort (aka denomsort)\n");
+    printf("    -rx: Radixsort (LSD)\n");
+}
+
+
 int main(int argc, char *argv[]) {
-    test_radix(24, 0, 100000000, 1000000, 24);
+    uint32_t min;
+    uint32_t max;
+    uint32_t inc;
+    if (argc != 6) 
+        print_help();
 }
